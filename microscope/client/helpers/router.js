b7fb73c8c5b8233c9c1509a203d3960eec259bfd
Andy Dai <daikeren@gmail.com>
2013-06-19 08:30:33 +0800

    Commit until 9.5

---
+++ microscope/client/helpers/router.js
@@ -0,0 +1,37 @@
+Meteor.Router.add({
+    '/': 'postsList',
+
+    '/posts/:_id': {
+        to: 'postPage',
+        and: function(id) {
+            Session.set('currentPostId', id);
+        }
+    },
+
+    '/posts/:_id/edit': {
+        to: 'postEdit',
+        and: function(id) {
+            Session.set('currentPostId', id);
+        }
+    },
+
+    '/submit': 'postSubmit'
+});
+
+Meteor.Router.filters({
+    'requireLogin': function(page) {
+        if (Meteor.user())
+            return page;
+        else if (Meteor.loggingIn())
+            return 'loading';
+        else
+            return 'accessDenied';
+    },
+    'clearErrors': function(page) {
+        clearErrors();
+        return page;
+    }
+});
+
+Meteor.Router.filter('requireLogin', {only: 'postSubmit'});
+Meteor.Router.filter('clearErrors');
